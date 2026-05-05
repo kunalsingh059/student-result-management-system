a04532b59473af51a4f121f9235e667e6b08d1ed
Kunal Singh Chouhan <kunalsinghchouhan07@gmail.com>
2026-05-05 13:57:53 +0530

    fix: added resultapp urls file

--- resultapp/urls.py
+++ resultapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # or home if you use that name
+    path('', views.index, name='home'),  # or home if you use that name
 ]
